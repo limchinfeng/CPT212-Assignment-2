e1ecf741081da8c89c2ccde9b91b2666ffdb85a0
supershuaifeng <limchinfeng@gmail.com>
2023-06-23 18:08:47 +0800

    Modified to Pseudocode Form

--- pseudocf.java
+++ pseudocf.java
@@ -3,15 +3,13 @@ import java.util.*;
 class pseudocf {
     private LinkedList<Integer> adjLists[];
     private int num[];
-    private List<Integer> edges;
-    private int i;
+    private List<Integer> path;
 
     // Graph creation
     pseudocf(int vertices) {
         adjLists = new LinkedList[vertices];
         num = new int[vertices];
-        edges = new ArrayList<>();
-        i = 0;
+        path = new ArrayList<>();
 
         for (int j = 0; j < vertices; j++)
             adjLists[j] = new LinkedList<Integer>();
@@ -26,45 +24,49 @@ class pseudocf {
     void findPathDFS(int vertex, int destination) {
         for (int j = 0; j < adjLists.length; j++)
             num[j] = 0;
-        edges = new ArrayList<>();
-        i = 0;
+        path = new ArrayList<>();
 
         DFS(vertex, destination);
 
-        if (num[destination] != 0) {
+        if (!path.isEmpty()) {
             System.out.println("Shortest path to the destination vertex:");
-            for (int j = 0; j < edges.size(); j += 2) {
-                int src = edges.get(j);
-                int dest = edges.get(j + 1);
-                System.out.println(src + " -> " + dest);
+            for (int j = 0; j < path.size(); j++) {
+                System.out.print(path.get(j));
+                if (j != path.size() - 1)
+                    System.out.print(" -> ");
             }
+            System.out.println();
         } else {
             System.out.println("No path found to the destination vertex.");
         }
     }
 
     // DFS algorithm
-    void DFS(int vertex, int destination) {
-        num[vertex] = i++;
+    boolean DFS(int vertex, int destination) {
+        num[vertex] = 1;
+
+        if (vertex == destination) {
+            path.add(vertex);
+            return true;
+        }
 
         Iterator<Integer> ite = adjLists[vertex].listIterator();
 
         while (ite.hasNext()) {
             int adj = ite.next();
             if (num[adj] == 0) {
-                edges.add(vertex);
-                edges.add(adj);
-                if (adj == destination) {
-                    num[destination] = i++; // Update destination vertex's num value
-                    return;
+                if (DFS(adj, destination)) {
+                    path.add(0, vertex);
+                    return true;
                 }
-                DFS(adj, destination);
             }
         }
+
+        return false;
     }
 
     public static void main(String args[]) {
-        pseudocf g = new pseudocf(9);
+        pseudocf g = new pseudocf(11);
 
         g.addEdge(0, 1);
         // g.addEdge(0, 2);
@@ -73,6 +75,12 @@ class pseudocf {
         g.addEdge(2, 3);
         g.addEdge(6, 7);
         g.addEdge(7, 8);
+        g.addEdge(2, 9);
+        g.addEdge(8, 9);
+        g.addEdge(9, 10);
+        g.addEdge(3, 5);
+        g.addEdge(5, 4);
+        
 
         System.out.println("Following is Depth First Traversal");
 
